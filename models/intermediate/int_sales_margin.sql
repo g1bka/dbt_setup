857ecc7d78fdad5df7ac9147add62ebfd1fb027d
Gabriela Georgieva <gabi.test.138@gmail.com>
2023-10-30 15:37:31 +0000

    V

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,6 @@
-SELECT 
+WITH int_sales_margin AS 
+
+(SELECT 
 
     p.products_id, 
     s.date_date, 
@@ -12,4 +14,6 @@ SELECT
 FROM {{ ref('stg_raw__sales') }} as s
 LEFT JOIN  {{ ref('stg_raw__product') }} as p
 
-ON p.products_id=s.products_id
+ON p.products_id=s.products_id)
+
+SELECT * FROM int_sales_margin
