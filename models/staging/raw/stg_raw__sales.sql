48673193ab1e9db128360ea50c0e0b7b9b53780f
Gabriela Georgieva <gabi.test.138@gmail.com>
2023-10-30 15:16:13 +0000

    c

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -10,11 +10,11 @@ renamed as (
 
     select
         concat(orders_id, " - " ,pdt_id) as sales_id,
-        date_date,
-        orders_id,
-        pdt_id as products_id,
+        CAST(date_date as date) date_date,
+        CAST(orders_id as float64) order_id,
+        CAST(pdt_id as float64) products_id,
         CAST(revenue as float64) revenue,
-        CAST(quantity as int64) as quantity
+        CAST(quantity as int64) quantity
 
     from source
 
