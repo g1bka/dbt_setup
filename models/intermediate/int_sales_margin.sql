bbd76033cf0abbead9ab4fa0a5c5ed1bea6da8dc
Gabriela Georgieva <gabi.test.138@gmail.com>
2023-10-30 16:08:18 +0000

    commit

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -10,9 +10,9 @@ int_sales_margin AS
     s.orders_id,
     s.revenue, 
     s.quantity, 
-    p.purchase_price--,
-    --ROUND(s.quantity * p.purchase_price,2) AS purchase_cost --,
-    -- s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
+    p.purchase_price,
+    ROUND(s.quantity * p.purchase_price,2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*p.purchase_price,2) AS margin
 
 FROM {{ ref('stg_raw__sales') }} as s
 LEFT JOIN  {{ ref('stg_raw__product') }} as p
